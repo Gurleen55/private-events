b4cfd408bcc02773b546a755018874b244d440f6
Gurleen55 <gurleencan6@gmail.com>
2025-01-26 11:58:39 -0400

    seperate events according to past and upcoming

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,15 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User", foreign_key: "user_id"
   has_many :event_attendees, foreign_key: :attended_event_id
   has_many :attendees, through: :event_attendees, source: :attendee
+
+  # def self.past
+  #   self.where(date: ..Date.today)
+  # end
+
+  # def self.future
+  #   self.where(date: Date.today..)
+  # end
+
+  scope :past, -> { where(date: ..Date.today) }
+  scope :future, -> { where(date: Date.today..) }
 end
